c46036b7c8709cb8a10caae4440a936a961f38ea
Ezra Lee <ezra.made.me@gmail.com>
2022-11-24 17:47:53 +0900

    221124

--- app/src/main/java/com/ezralee/bdotodo/goallist/GoalPresetActivity.kt
+++ app/src/main/java/com/ezralee/bdotodo/goallist/GoalPresetActivity.kt
@@ -1,30 +1,63 @@
 package com.ezralee.bdotodo.goallist
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.ezralee.bdotodo.R
 import com.ezralee.bdotodo.history.GoalPresetItem
 import com.ezralee.bdotodo.databinding.ActivityGoalPresetBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class GoalPresetActivity: AppCompatActivity() {
+class GoalPresetActivity: AppCompatActivity(),
+    BottomNavigationView.OnNavigationItemSelectedListener {
 
     val binding: ActivityGoalPresetBinding by lazy { ActivityGoalPresetBinding.inflate(layoutInflater) }
-    var goalFragments: MutableList<Fragment> = mutableListOf(SetGoalFragment1(), SetGoalFragment2())
-    var items : MutableList<GoalPresetItem> = mutableListOf(
-        GoalPresetItem("오네트의 정령수"),
-        GoalPresetItem("오도어의 정령수"),
-        GoalPresetItem("라피 머시기의 나침반"),
-        GoalPresetItem("고고학자의 지도"),
-        GoalPresetItem("중범선-비상"),
-        GoalPresetItem("크로그달로의 마구-바다")
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.goallistPresetRecycler.adapter = GoalPresetAdapter(this@GoalPresetActivity,items)
+        binding.presetBNV.setOnNavigationItemSelectedListener(this)
+        binding.presetVP.adapter = GoalPresetViewPagerAdapter(supportFragmentManager,lifecycle)
+        binding.presetVP.registerOnPageChangeCallback(ViewPagerPageChangeCallback())
 
+        binding.close.setOnClickListener {
+            finish()
+        }
+
+    }
+
+    //선택된 BNV 감지하는 리스너 오버라이딩
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.menu_treasure -> {
+                binding.presetVP.currentItem = 0
+                true
+            }
+            R.id.menu_lifeskill -> {
+                binding.presetVP.currentItem = 1
+                true
+            }
+            R.id.menu_gear -> {
+                binding.presetVP.currentItem = 2
+                true
+            }
+            else -> false
+        }
+    }
+
+    //ViewPager의 페이지 변화 감지 콜백메서드 오버라이딩
+    inner class ViewPagerPageChangeCallback: ViewPager2.OnPageChangeCallback(){
+        override fun onPageSelected(position: Int) {
+            binding.presetBNV.selectedItemId = when(position){
+                0 -> R.id.menu_treasure
+                1 -> R.id.menu_lifeskill
+                2 -> R.id.menu_gear
+                else -> error("no menu")
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/ezralee/bdotodo/goallist/GoalPresetListFragment.kt
@@ -0,0 +1,35 @@
+package com.ezralee.bdotodo.goallist
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ezralee.bdotodo.databinding.FragmentPresetListBinding
+import com.ezralee.bdotodo.history.GoalPresetItem
+
+class GoalPresetListFragment(): Fragment() {
+    var items: MutableList<GoalPresetItem> = mutableListOf(
+        GoalPresetItem("오네트의 정령수"),
+        GoalPresetItem("오도어의 정령수"),
+        GoalPresetItem("라피 머시기의 나침반"),
+        GoalPresetItem("고고학자의 지도"),
+        GoalPresetItem("중범선-비상"),
+        GoalPresetItem("크로그달로의 마구-바다")
+    )
+    val binding: FragmentPresetListBinding by lazy { FragmentPresetListBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.goallistPresetRecycler.adapter = GoalPresetAdapter(activity as GoalPresetActivity,items)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+}
